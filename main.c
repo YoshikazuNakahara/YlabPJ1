c6777d24796d1908095e187ed246204a59d10d29
yoshiNakaharakazu <o231053y@mail.kyutech.jp>
2019-04-04 00:55:01 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,33 +2,42 @@
 #include <stdlib.h>
 #include <string.h>
 
+// check corresponding parentheses
 int parenthesesChecker(char*);
 
 int main(void){
-    // Your code here!
-    // ファイルのオープン
-    char buffer[256];t
+    char buffer[256]; // a string in the file 
     char* string;
     FILE *fp;
-    char input[256];
+    char input[256]; // file name
     
-    printf("input string:");
+    printf("Input a file name which is ascii string and length of 256:\n");
     scanf(&s, &input);
     fp = fopen(input, "r");
-    if (fp == NULL) {                             // オープンに失敗した場合
-        printf("Cannot open the file.¥n");            // エラーメッセージを出して
-        exit(1);                                    // 異常終了
+    
+    // openning the file failed
+    if (fp == NULL) {                             
+        printf("Cannot open the file.\n");            
+        exit(1);                                    
     }
     
-    for (int i = 0; (buffer[i] = fgetc( fp )) != EOF;i++);
+    // input string in the file into a string buffer.
+    for (int i = 0; (buffer[i] = fgetc( fp )) != EOF && strlen(buffer[i]) < 257;i++);
+    // string in the file > 256
+    if (strlen(buffer[i]) >=){
+        printf("String in the file is longer than 256.\n");
+        exit(1);
+    }
     
     int flag = parenthesesChecker(buffer);
+    
+    // output whether correspond parentheses or not
     if (flag)
-        printf("Not correspond parentheses.");
+        printf("Not correspond parentheses.\n");
     else
-        printf("Correspond parentheses.");
-    // ファイルのクローズ
-    //fclose(fp);
+        printf("Correspond parentheses.\n");
+    // Close the file.
+    fclose(fp);
     
     return 0;
 }
