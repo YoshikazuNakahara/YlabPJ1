85c5a689e1664ca7cfe0332f4788b33aca48dfbf
yoshiNakaharakazu <o231053y@mail.kyutech.jp>
2019-04-09 00:52:36 +0900

    Create rec2.c

---
+++ rec2.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int checkBrackets(char*);
+
+int main(void){
+    // Your code here!
+    // ファイルのオープン
+    int flag = 1;
+    flag = checkBrackets("(())");
+    //flag = parenthesesChecker("(l)");
+    if (flag)
+        printf("Complete parentheses.");
+    else
+        printf("Not complete parentheses.");
+}
+
+int checkBrackets(char* string){
+    char sub_string[256];
+    printf("string: %s\n", string);
+    int length = strlen(string);
+    if (length == 0) {
+            return 1;
+    }
+    else {
+        int left, right;
+        int i;
+        int flag = 0;
+        for (left = 0;left < length;i++){
+           if (string[left] == '(') {
+               break;
+           }
+           else if (string[left] == ')'){
+               return 0;
+           }
+        }
+        for (right = length - 1;right > - 1;right--){
+           if (string[right] == ')') {
+               break;
+           }
+           else if (string[right] == '('){
+               return 0;
+           }
+        }
+        for (i = left + 1;i < right;i++){
+            sub_string[i] = string[i];
+        }
+        printf("sub_string: %s\n", sub_string);
+        //return checkBrackets(sub_string);
+        return 1;
+    }
+}
