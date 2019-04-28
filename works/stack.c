3aab965f73722f967cdfd00bbb0858a581aa37c7
Yoshikazu Nakahara <o231053y@mail.kyutech.ac.jp>
2019-04-28 16:45:17 +0900

    Devide functions.

--- works/stack.c
+++ works/stack.c
@@ -39,38 +39,3 @@ void printStack(Stack* sp){
         printf("Stack Empty.\n");
     }
 }
-
-int checkParentheses(FILE* fp){
-    char c;
-    Stack st;
-    Stack* sp = &st;
-    
-    makeStack(sp);
-    
-    for (int i = 0; (c = fgetc( fp )) != EOF;i++){
-        if (c == '('){
-           pushStack(sp, 1);
-        }
-        else if (c == ')' ){
-           if (popStack(sp) != 1)
-               return 0;
-        } 
-        else if (c == '['){
-           pushStack(sp, 2);
-        }
-        else if (c == ']' ){
-           if (popStack(sp) != 2)
-               return 0;
-        } 
-        else if (c == '{'){
-           pushStack(sp, 3);
-        }
-        else if (c == '}' ){
-           if (popStack(sp) != 3)
-               return 0;
-        } 
-
-    }
-    //printStack(sp);
-    return (isEmptyStack(sp));
-}
