504e6b6a13af9546e6fc7f91e19357c4cfb3a749
Yoshikazu Nakahara <o231053y@mail.kyutech.jp>
2019-05-16 00:29:17 +0900

    Rename checkKeywords directory to keywords directory.

--- src/checkKeywords/keywords.h
+++
@@ -1,8 +0,0 @@
-#define WORDCOUNT 2
-#define KEYWORDS_MAX_LENGTH 4
-typedef struct {
-  char left[WORDCOUNT+1][KEYWORDS_MAX_LENGTH+1];
-  char right[WORDCOUNT+1][KEYWORDS_MAX_LENGTH+1];
-} Keywords;
-
-int* analizeKeywords(FILE *);
